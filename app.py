e164a8a0a9d080e383b9fa0f1e047a8164a7ed85
vishals9711 <vishalsharma1907@gmail.com>
2021-05-02 22:35:55 +0530

    API changes and added template folder

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 from searchAPI import returnSearch,returnMovieDetailsResponse,returnTVShowDetailsResponse
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='./templates',static_url_path='/')
 BASE_ROUTE = '/api/v1'
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
 @app.route(BASE_ROUTE + '/<string:name>')
 def search(name):
     return returnSearch(name)
--- searchAPI.py
+++ searchAPI.py
@@ -18,12 +18,14 @@ def returnMovieDetailsResponse(id:str):
     url = 'https://api.themoviedb.org/3/movie/' + id
     payload = {
         'api_key':"3b9a6ed72d9f555baf1e1ed7824b1314",
-        'append_to_response':"watch/providers,recommendations,credits"
+        'append_to_response':"recommendations,credits"
     }
     response = requests.get(url, params=payload).json()
     top6Cast = response['credits']['cast'][:6]
     response['credits']['cast'] = top6Cast
     response['credits']['crew']= []
+    response['ott-details']  = (returnOTTDetails(response['imdb_id'])['streamingAvailability']['country'])
+    response['providers'] = returnWatchProviders()
     return response
 
 def returnTVShowDetailsResponse(id:int):
@@ -39,6 +41,18 @@ def returnTVShowDetailsResponse(id:int):
     response['credits']['crew']= []
     return response
 
+
+def returnOTTDetails(imdb:str):
+    url = 'https://ott-details.p.rapidapi.com/gettitleDetails'
+    headers = {
+    'x-rapidapi-key': "f5e16aaab0mshd5fc55058b588b7p19c432jsn2af2569eef34",
+    'x-rapidapi-host': "ott-details.p.rapidapi.com"
+    }
+    querystring = {"imdbid":imdb}
+    response = requests.get(url, headers=headers, params=querystring).json()
+    return response
+
+
 def remove_none(array):
     result = filter(lambda v: remove_non_obj(v), array)
     return list(result)
@@ -48,3 +62,12 @@ def remove_non_obj(obj):
     obj.clear()
     obj.update(filtered)
     return obj
+
+def returnWatchProviders():
+    url = 'https://api.themoviedb.org/3/watch/providers/movie'
+    payload = {
+        'api_key':"3b9a6ed72d9f555baf1e1ed7824b1314",
+        'watch_region':"IN"
+    }
+    response = requests.get(url, params=payload).json()
+    return response
